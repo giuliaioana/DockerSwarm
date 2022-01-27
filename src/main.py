3371c7d86faf1704109f207fb4692bcb3fbedb76
Giulia <giuliapirgariu@yahoo.com>
2022-01-27 03:05:20 +0200

    as

--- src/main.py
+++ src/main.py
@@ -25,10 +25,10 @@ rabitmq_host = "54.195.175.251" if os.getenv("SWARM") else "rabbitmq"
 
 api = Flask(__name__)
 
-host= "54.195.175.251" if os.getenv("SWARM") else settings.hostname
+host = "54.195.175.251" if os.getenv("SWARM") else settings.hostname
 print(f"""mysql://{settings.user}:{str(get_db_password())}@{host}/{settings.db}""")
-api.config['SQLALCHEMY_DATABASE_URI'] = f"""mysql://{settings.user}:{get_db_password()}@{host}/{settings.db}"""
-
+#api.config['SQLALCHEMY_DATABASE_URI'] = f"""mysql://{settings.user}:{get_db_password()}@{host}/{settings.db}"""
+api.config['SQLALCHEMY_DATABASE_URI'] = "mysql://admin:admin@54.195.175.251/main"
 db = SQLAlchemy(api)
 class Products(db.Model):
     __tablename__='Products'
