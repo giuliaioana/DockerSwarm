768daa08f370f929daf6f5098d232a9309bb0079
Giulia <giuliapirgariu@yahoo.com>
2022-01-27 02:58:30 +0200

    add changes

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ rabitmq_host = "54.195.175.251" if os.getenv("SWARM") else "rabbitmq"
 api = Flask(__name__)
 
 host= "54.195.175.251" if os.getenv("SWARM") else settings.hostname
-
+print(f'mysql://{settings.user}:{str(get_db_password())}@{host}/{settings.db}')
 api.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{settings.user}:{str(get_db_password())}@{host}/{settings.db}'
 
 db = SQLAlchemy(api)
